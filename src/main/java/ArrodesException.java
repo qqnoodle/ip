35f2a0b78b45cddf011581f0a4bb8045c216b399
qqnoodle <foongqiming@gmail.com>
2026-08-18 10:38:54 +0800

    Add more default known error message

--- src/main/java/ArrodesException.java
+++ src/main/java/ArrodesException.java
@@ -1,8 +1,10 @@
 public class ArrodesException extends RuntimeException {
-
+    public static final String NO_INPUT = "Arrodes cannot decipher your intentions...";
     public static final String UNKNOWN_COMMAND = "Arrodes has not learn to process this request!!";
     public static final String EMPTY_DESCRIPTION = "Arrodes requires more information of the nature of the task";
-    public static final String INSUFFICIENT_PARAMS = "Arrodes has not learn to process this request!!";
+    public static final String INCORRECT_PARAMS_COUNT = "Arrodes requires more magic words (parameters) for the request!!";
+    public static final String INCORRECT_PARAMS = "Your ritualistic magic parameters is incorrect!!!";
+    public static final String TASK_LIST_FULL = "Records of Arrodes reached its limit and can no longer accept more request!!!";
 
     /**
      * Constructs a new ArrodesException with the default RuntimeException
