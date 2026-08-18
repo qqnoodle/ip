fcfbfc6dcbfa5973062e9845bb44347cdbc2cffd
qqnoodle <foongqiming@gmail.com>
2026-08-18 13:50:14 +0800

    Add TaskList class to handle Task objects

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private int capacity;
+    ArrayList<Task> list = new ArrayList<>();
+
+    public TaskList() {
+        //Default capacity 100
+        this.capacity = 100;
+    }
+    public TaskList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public boolean isFull() {
+        return list.size() == capacity;
+    }
+
+    public void insert(Task task) throws ArrodesException{
+        if (isFull()) throw new ArrodesException(ArrodesException.TASK_LIST_FULL);
+        list.add(task);
+    }
+
+    public void delete(int itemNumber) throws ArrodesException{
+        int itemIndex = itemNumber - 1;
+        if ((itemIndex) >= list.size() || itemIndex < 0) throw new ArrodesException(ArrodesException.ITEM_NOT_IN_LIST);
+        list.remove(itemIndex);
+    }
+}
