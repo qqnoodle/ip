f95c242c52565bbd315b6bae6b71e0d50104a5cf
qqnoodle <foongqiming@gmail.com>
2026-09-07 21:27:20 +0800

    Assert valid storage records produce tasks

--- src/main/java/Arrodes/storage/Storage.java
+++ src/main/java/Arrodes/storage/Storage.java
@@ -172,6 +172,7 @@ public class Storage {
         } else if (!"0".equals(fields.get(1))) {
             throw invalidRecord();
         }
+        assert task != null : "A valid storage record must produce a task.";
         return task;
     }
 
